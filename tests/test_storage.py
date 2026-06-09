798310dd58e7216a677d3b36c9a33c3d2629a4a6
NapstaTT <82403339+NapstaTT@users.noreply.github.com>
2026-06-10 00:24:21 +0500

    Delete tests directory

--- tests/test_storage.py
+++
@@ -1,6 +0,0 @@
-from backend.storage import append_to_tmp, read_tmp, clear_tmp
-
-append_to_tmp("default", {"ver": 1, "role": "user", "content": "Привет"})
-print(read_tmp("default"))  # должно вывести список с одним словарём
-clear_tmp("default")
-print(read_tmp("default"))  # пустой список
